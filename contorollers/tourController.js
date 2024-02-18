3e5964a51664521a629d720b498bd9ca5d4d034c
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-02-18 12:54:07 +0100

    Delete a tour

--- contorollers/tourController.js
+++ contorollers/tourController.js
@@ -89,18 +89,19 @@ exports.updateTour = async (req, res) => {
     }
 }
 
-exports.deleteTour = (req, res) => {
-    
-    if(req.params.id *1>tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Tour not found'
-        });
+exports.deleteTour = async (req, res) => {
+    try{
+        await Tour.findByIdAndDelete(req.params.id)
+        
+        res.status(204).json({
+            status: 'OK',
+            data: null
+   })
+    }catch(err){
+
+    res.status(400).json({
+        status: 'fail',
+        message: err})
+   
     }
-    res.status(204).json({
-        status: 'OK',
-        data: null
-   }
-    
-   )
 }
