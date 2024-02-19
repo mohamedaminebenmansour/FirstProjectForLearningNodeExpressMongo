54ef935e96ecb64b0e0884d49966352c433ea8de
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-02-19 21:41:33 +0100

    Refatoring API Feaures

--- contorollers/tourController.js
+++ contorollers/tourController.js
@@ -1,6 +1,5 @@
-const fs = require('fs'); 
 const Tour = require('./../models/tourModel');
-const { query } = require('express');
+const APIFeatures = require('./../utils/apiFeatures');
 // 
 exports.aliasTopTours = (req, res, next) =>{
     req.query.limit ='5'
@@ -9,58 +8,6 @@ exports.aliasTopTours = (req, res, next) =>{
     next();
 }
 
-class APIFeatures {
-    constructor(query, queryString) {
-      this.query = query;
-      this.queryString = queryString;
-    }
-  
-    filter() {
-      const queryObj = { ...this.queryString };
-      const excludedFields = ['page', 'sort', 'limit', 'fields'];
-      excludedFields.forEach(el => delete queryObj[el]);
-  
-      // 1B) Advanced filtering
-      let queryStr = JSON.stringify(queryObj);
-      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-  
-      this.query = this.query.find(JSON.parse(queryStr));
-  
-      return this;
-    }
-  
-    sort() {
-      if (this.queryString.sort) {
-        const sortBy = this.queryString.sort.split(',').join(' ');
-        this.query = this.query.sort(sortBy);
-      } else {
-        this.query = this.query.sort('-createdAt');
-      }
-  
-      return this;
-    }
-  
-    limitFields() {
-      if (this.queryString.fields) {
-        const fields = this.queryString.fields.split(',').join(' ');
-        this.query = this.query.select(fields);
-      } else {
-        this.query = this.query.select('-__v');
-      }
-  
-      return this;
-    }
-  
-    paginate() {
-      const page = this.queryString.page * 1 || 1;
-      const limit = this.queryString.limit * 1 || 100;
-      const skip = (page - 1) * limit;
-  
-      this.query = this.query.skip(skip).limit(limit);
-  
-      return this;
-    }
-  }
 
 exports.getAllTours =async (req, res) => {
     try {
@@ -74,10 +21,10 @@ exports.getAllTours =async (req, res) => {
             
             status: 'OK',
             
-            results: tours.length, // Number of tours in the data
+            results: tours.length, 
             
             data: {
-                tours: tours // Sending the array of tours
+                tours: tours 
             }
         });
     } catch (err) {
