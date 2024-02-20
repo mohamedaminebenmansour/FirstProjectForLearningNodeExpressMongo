e7b1849667e9005fe4d77d97514ef5ba722163c2
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-02-20 10:44:19 +0100

    Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-const tourShema = new mongoose.Schema({
+const tourSchema = new mongoose.Schema({
     name: 
     {
       type: String,
@@ -63,7 +63,11 @@ const tourShema = new mongoose.Schema({
         type: Date,
         default: Date.now()
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour:{
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON:{virtuals :true},
@@ -80,26 +84,44 @@ these two fields in a database if we can easily convert one to the other */
 /*I used this regular function here because remember, an arrow function does not get its own "this" keyword.
 In here we actually need the "this" keyword because the disk keyword in this caseis going to be pointing to the current document.
 And so usually when we want to use this,then we should always use a regular function. */
-tourShema.virtual('durationWeeks').get(function(){
+tourSchema.virtual('durationWeeks').get(function(){
   return this.duration / 7;
 });
 
 //Document Middelware: runs before .save() and .create() not .insertMany()
-tourShema.pre('save', function(next){
+tourSchema.pre('save', function(next){
   this.slug = slugify(this.name, {lower:true});
   next();
 })
 /*
- tourShema.pre('save', function(next){
+ tourSchema.pre('save', function(next){
   console.log('wil save document ...');
   next();
  })
-tourShema.post('save', function(doc,next){
+tourSchema.post('save', function(doc,next){
   console.log(doc);
   next();
 })
 */
 
-const Tour = mongoose.model('Tour', tourShema);
+// QUERY MIDDLEWARE
+/*query middleware allows us to run functions before or after a certain query is executed.
+And so let's now add a pre-find hook, so basically, a middleware that is gonna run
+before any find query is executed.
+ */
+
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
