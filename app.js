9763a653040910f93a8e408acce481661950b860
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-02-20 18:35:39 +0100

    Implementing a Global Error Handling Middleware

--- app.js
+++ app.js
@@ -23,9 +23,28 @@ app.use('/api/v1/tours', tourRouter); //this is called mounting the router
 app.use('/api/v1/users', userRouter); //this is called mounting the router
 
 app.all('*', (req, res, next) => {
+/*
   res.status(404).json({
     status: 'fail',
     message: `Can't find ${req.originalUrl} on this server`
+  });
+  */
+ const err = new Error(`Can't find ${req.originalUrl} on this server`);
+ err.status= "fail";
+ err.statusCode = 404;
+ next(err);
+})
+
+
+
+//Headling middleware
+/*By specifying 4 parameteres Ewpress automaticaally knows that this entire function here is an error middleware */
+app.use((err, req, res, next) => {
+  err.statusCode =err.statusCode || 500;
+  err.status=err.status || "error"
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 })
 
