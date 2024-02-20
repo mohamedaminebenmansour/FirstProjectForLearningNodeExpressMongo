bd39704d1c781ee03b52a091f3e09aa6372a2064
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-02-20 09:30:29 +0100

    Virtual Properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -61,7 +61,26 @@ const tourShema = new mongoose.Schema({
         default: Date.now()
     },
     startDates: [Date]
+  },
+  {
+    toJSON:{virtuals :true},
+    toObject:{virtuals :true}
+    /*we cannot use this virtual property here in a query, because they're technically not part of the database.
+    So we can not say, for example,tour.find where duration weeks is equal to one. That's not gonna work, 
+    again because this property is not actually part of the database. */
   });
+/*Virtual properties are basically fields that we can define on our schema but that will not be persisted.
+So they will not be saved into the database in order to save us some space there.And most of the time, of course,
+we want to really save our data to the database, but virtual properties make a lot of sense for fields that 
+can be derived from one another. For example a conversion from miles to kilometers,it doesn't make sense to store 
+these two fields in a database if we can easily convert one to the other */
+/*I used this regular function here because remember, an arrow function does not get its own "this" keyword.
+In here we actually need the "this" keyword because the disk keyword in this caseis going to be pointing to the current document.
+And so usually when we want to use this,then we should always use a regular function. */
+tourShema.virtual('durationWeeks').get(function(){
+  return this.duration / 7;
+});
+
 const Tour = mongoose.model('Tour', tourShema);
 
 module.exports = Tour;
