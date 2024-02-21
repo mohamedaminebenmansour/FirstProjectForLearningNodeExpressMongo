4da9c4062eeff30b6e94c2eac9cb1bff897fc979
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-02-21 19:24:00 +0100

    Handling Invalid Database Ids

--- app.js
+++ app.js
@@ -8,41 +8,29 @@ const globalErrorHandler = require('./contorollers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`)); //we can serve static file from a folder and not route
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// 2)ROUTE HADNLERS
-
-app.use('/api/v1/tours', tourRouter); //this is called mounting the router
-app.use('/api/v1/users', userRouter); //this is called mounting the router
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-/*
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`
-  });
-  */
- //const err = new Error(`Can't find ${req.originalUrl} on this server`);
- //err.status= "fail";
- //err.statusCode = 404;
- next(new AppError(`Can't find ${req.originalUrl} on this server ! `),404);
-})
+  console.log("I am in app.js");
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
-
-
-//Headling middleware
-/*By specifying 4 parameteres Ewpress automaticaally knows that this entire function here is an error middleware */
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 module.exports = app;
--- contorollers/tourController.js
+++ contorollers/tourController.js
@@ -41,7 +41,7 @@ exports.getTour =catchAsync ( async (req, res,next) => {
 })
 
 
-exports.createTour = catchAsync(async (req, res) => {
+exports.createTour = catchAsync(async (req, res,next) => {
     const newTour = await Tour.create(req.body);//"req.body" that's the data that comes with the post request
         res.status(201).json({
             status: 'ok',
