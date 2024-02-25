4fd79782b0e7571c9dce2862bc5f7dd15e18fe08
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-02-25 22:18:38 +0100

    Added middleware for populating user and tour fields in reviews
    Implemented a Mongoose middleware on the review schema to automatically populate the 'user' and 'tour' fields whenever a query is executed using the 'find' method. This middleware enhances the response by including additional information about the user (name) and tour (name, photo) associated with each review.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,18 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function(next) {
+  this.populate({
+    path:"user",
+    select:'name'
+  });
+  this.populate({
+    path:"tour",
+    select:'name photo'
+  });
+  next();
+})
+
 
 
 
