7a1cc6772d6ba83aa8e9e622cc9a1197d755c72d
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-02-25 10:58:08 +0100

    Implemented Tour Guides Embedding Model and middleware for guide handling.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,132 +1,139 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 //const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
-    name: 
-    {
-      type: String,
-      required: [ true , "A tour must have a name " ],
-      unique: true,
-      trim: true,
-      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
-      minlength: [10, 'A tour name must have more or equal then 10 characters']
-      // validate: [validator.isAlpha, 'Tour name must only contain characters']
+  name: 
+  {
+    type: String,
+    required: [ true , "A tour must have a name " ],
+    unique: true,
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [10, 'A tour name must have more or equal then 10 characters']
+    // validate: [validator.isAlpha, 'Tour name must only contain characters']
 
-    },
-    slug: String,
-    duration: 
-    {
-        type: Number,
-        required: [ true, "A tour must have a duration" ]
-    },
-    maxGroupSize: 
-    {
-        type: Number,
-        required: [ true, "A tour must have a maxGroupSize" ]
-    },
-    difficulty: 
-    {
-      type: String,
-      required: [ true, "A tour must have a difficulty" ],
-      enum: {// only for Strings
-        values: ['easy', 'medium', 'difficult'],
-        message: 'Difficulty is either: easy, medium, difficult'
-      }
-    },
-    rating: 
-    {
-      type : Number,
-      default:4.5,
-      min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0']
-    },
-    ratingsAverge: 
-    {
-      type : Number,
-      default:4.5,
-      min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0']
-    },
-    ratingsQuantity: {
-      type : Number,
-      default:4.5
-    },
-    price: {
+  },
+  slug: String,
+  duration: 
+  {
       type: Number,
-      required: [ true , "A tour must have a price" ]
-    },
-    priceDiscount: {
+      required: [ true, "A tour must have a duration" ]
+  },
+  maxGroupSize: 
+  {
       type: Number,
-      validate : function(val){
-        //this only points to current doc on NEW document creation
-        return val < this.price; 
-      },
-      message: 'Discount price ({VALUE}) should be below regular price'
-    },
-    summary :{
-        type: String,
-        required: true,
-        trim: true
-    },
-    description: {
-        type: String,
-        trim: true
-    },
-    imageCover: {
-        type : String,
-        required: [true,'A tour must have a cover image']
-    },
-    image: [String],
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    },
-    startDates: [Date],
-    secretTour:{
-      type: Boolean,
-      default: false
+      required: [ true, "A tour must have a maxGroupSize" ]
+  },
+  difficulty: 
+  {
+    type: String,
+    required: [ true, "A tour must have a difficulty" ],
+    enum: {// only for Strings
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
     }
   },
+  rating: 
   {
-    startLocatoon:{//new object with tow fields
-      //GeoJSON
+    type : Number,
+    default:4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0']
+  },
+  ratingsAverge: 
+  {
+    type : Number,
+    default:4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0']
+  },
+  ratingsQuantity: {
+    type : Number,
+    default:4.5
+  },
+  price: {
+    type: Number,
+    required: [ true , "A tour must have a price" ]
+  },
+  priceDiscount: {
+    type: Number,
+    validate : function(val){
+      //this only points to current doc on NEW document creation
+      return val < this.price; 
+    },
+    message: 'Discount price ({VALUE}) should be below regular price'
+  },
+  summary :{
+      type: String,
+      required: true,
+      trim: true
+  },
+  description: {
+      type: String,
+      trim: true
+  },
+  imageCover: {
+      type : String,
+      required: [true,'A tour must have a cover image']
+  },
+  image: [String],
+  createdAt: {
+      type: Date,
+      default: Date.now()
+  },
+  startDates: [Date],
+  secretTour:{
+    type: Boolean,
+    default: false
+  }
+,
+
+  startLocatoon:{//new object with tow fields
+    //GeoJSON
+    type: {
+      type: String,
+      enum: ['Point'],
+      default: 'Point'
+    },
+    coordinates: {
+      type: [Number],//that basically means the coordinates that we expect an array of numbers
+      address: String,
+      description: String
+    }
+  },
+  /*In order to really create new documents and theb embed them into another document
+  We actually need to create an array*/
+  
+  locations: [
+    {
       type: {
         type: String,
-        enum: ['Point'],
-        default: 'Point'
+        default: 'Point',
+        enum: ['Point']
       },
-      coordinates: {
-        type: [Number],//that basically means the coordinates that we expect an array of numbers
-        address: String,
-        description: String
-      }
-    },
-    /*In order to really create new documents and theb embed them into another document
-    We actually need to create an array*/
-    
-    locations: [
-      {
-        type: {
-          type: String,
-          default: 'Point',
-          enum: ['Point']
-        },
-        coordinates: [Number],
-        address: String,
-        description: String,
-        day: Number
-      }
-    ],
-  },
-  
-  {
-    toJSON:{virtuals :true},
-    toObject:{virtuals :true}
-    /*we cannot use this virtual property here in a query, because they're technically not part of the database.
-    So we can not say, for example,tour.find where duration weeks is equal to one. That's not gonna work, 
-    again because this property is not actually part of the database. */
-  });
+      coordinates: [Number],
+      address: String,
+      description: String,
+      day: Number
+    }
+  ],
+  guides: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User'
+    }
+  ]
+},
+
+{
+  toJSON:{virtuals :true},
+  toObject:{virtuals :true}
+  /*we cannot use this virtual property here in a query, because they're technically not part of the database.
+  So we can not say, for example,tour.find where duration weeks is equal to one. That's not gonna work, 
+  again because this property is not actually part of the database. */
+});
 /*Virtual properties are basically fields that we can define on our schema but that will not be persisted.
 So they will not be saved into the database in order to save us some space there.And most of the time, of course,
 we want to really save our data to the database, but virtual properties make a lot of sense for fields that 
@@ -144,6 +151,18 @@ tourSchema.pre('save', function(next){
   this.slug = slugify(this.name, {lower:true});
   next();
 })
+
+tourSchema.pre('save', async function(next) {
+  console.log(this)
+  if (!this.guides) {
+    return next(); // or handle this case as needed
+  }
+
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
+
 /*
  tourSchema.pre('save', function(next){
   console.log('wil save document ...');
