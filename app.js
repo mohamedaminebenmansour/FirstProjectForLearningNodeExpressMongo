28cf007e421330212ac1a09fa4d2d944b456707b
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-03-01 20:11:02 +0100

    Setting up Pug In Express

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+const path = require('path');
 const express = require('express');
 // Importing Express.js framework
-const app = express(); // Creating an instance of Express application
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -15,6 +15,14 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
+const app = express(); // Creating an instance of Express application
+
+app.set('view engine','pug');
+app.set('views',path.join(__dirname, 'views'))
+
+//Serving static files
+//app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 1) Global MIDDLEWARES
 //set security HTTP headers
@@ -62,8 +70,7 @@ app.use(hpp({
     'price'
   ]
 }));
-//Serving static files
-app.use(express.static(`${__dirname}/public`));
+
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -72,6 +79,9 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
--- contorollers/authController.js
+++ contorollers/authController.js
@@ -65,6 +65,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 });
 
 exports.login = catchAsync(async (req, res, next) => {
+  
     const {email,password}= req.body;
     // 1) Check if email and password exist
     if(!email || !password){
