85fc6cf4cedc56f181bfe46f9947561d9937c799
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-03-03 10:53:37 +0100

    Refactor tour card layout and styling

--- contorollers/viewsController.js
+++ contorollers/viewsController.js
@@ -1,11 +1,30 @@
-exports.getOverview = (req, res) => {
-    res.status(200).render('overview', {
-        tour: 'this is my exemple',
-        'user': "mohamed amin benmansour"
-    });
-}
+const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.getTour =  (req, res) => {
+exports.getOverview =catchAsync(async (req, res) => {
+    //1) Get tour data from collection
+    const tours = await Tour.find();
+    //2)Building template
+
+    //3) Render template using data from 
+    res.status(200).render('overview', {
+        title: 'All tours',
+        tours: tours
+    });
+})
+
+exports.getTour = async  (req, res) => {
+    // 1) Get the data, for the requested tour (inslucding reviews and guides).
+    const tour = await Tour.findOne({slug: req.params.slug})
+        .populate({
+            path: 'reviews',
+            fileds:'review rating user'
+        });
+    //2) Build template
+
+    //3) Render template using data from
     res.status(200).render('tour',{
       title: "tour"
     });
