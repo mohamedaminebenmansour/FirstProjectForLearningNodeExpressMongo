5e8e7925e21c02f38dcb24f699839e615b4c0d34
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-03-03 18:59:58 +0100

    Building The Tour Overview

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -28,9 +28,10 @@ const tours= JSON.parse(fs.readFileSync(`${__dirname}/tours.json`,'utf-8'));
 const users= JSON.parse(fs.readFileSync(`${__dirname}/users.json`,'utf-8'));
 const reviews= JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`,'utf-8'));
 //Import Data Into MongoDB
+console.log(tours)
 const importData = async () =>{
     try {
-        await Tour.create(tours);
+        await Tour.create(tours,{validateBeforeSave:false});
         await User.create(users,{validateBeforeSave:false});
         await Review.create(reviews);
         console.log("Data Imported");
